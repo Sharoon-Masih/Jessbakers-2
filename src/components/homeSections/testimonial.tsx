1b0f8be9bd10f252081a979b3228990347a2fa57
Sharoon-Masih <sharoonmasih43@gmail.com>
2024-05-25 22:53:28 +0500

    Update testimonial.tsx

--- src/components/homeSections/testimonial.tsx
+++ src/components/homeSections/testimonial.tsx
@@ -13,16 +13,16 @@ const Testimonial = () => {
                         <Image src={'/openQuote.svg'} fill={true} alt='OpenQuote' />
                     </div>
                     <p className='pt-[60px] lg:pt-0 text-[18px] sm:text-[28px] text-[#272727] text-center font-semibold lg:text-[48px] z-10 lg:w-[977px] lg:h-[180px]'>the eggless cakes here are really good. Had ordered a kit kat cake which was really good. Surely worth a try.</p>
-                    <div className= 'w-[90px] h-[80px] lg:w-[142px] lg:h-[98px] absolute top-[234px] right-[102px]'>
+                    <div className='w-[90px] h-[80px] lg:w-[142px] lg:h-[98px] absolute top-[234px] right-[102px]'>
                         <Image src={'/closeQuote.svg'} fill={true} alt='CloseQuote' />
                     </div>
                 </div>
-                <div className='flex flex-col gap-5 items-center justify-between'> 
-                <div className='w-[88px] h-[88px] rounded-full border relative overflow-hidden '>
-                  <Image src={"/output-onlinetools.png"} fill={true} alt='Sharoon Masih' className='object-cover object-center'/>
-                </div>
-                <strong className='text-[24px] font-semibold '>Sharoon Masih</strong>
-                <strong className='text-[#272727] font-medium text-18px -mt-3'>CO-Founder at Techverse51</strong>
+                <div className='flex flex-col gap-5 items-center justify-between'>
+                    <div className='w-[88px] h-[88px] rounded-full border relative overflow-hidden '>
+                        <Image src={"/output-onlinetools.png"} fill={true} alt='Sharoon Masih' className='object-cover object-center' />
+                    </div>
+                    <strong className='text-[24px] font-semibold '>Sharoon Masih</strong>
+                    <strong className='text-[#272727] font-medium text-18px -mt-3'>CO-Founder at Techverse51</strong>
                 </div>
             </div>
         </section>
