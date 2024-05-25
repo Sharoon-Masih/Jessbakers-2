832613f1165db24b48a4be78511b51f50c0fc406
Sharoon-Masih <sharoonmasih43@gmail.com>
2024-05-25 13:40:15 +0500

    Update quote.tsx

--- src/components/homeSections/quote.tsx
+++ src/components/homeSections/quote.tsx
@@ -8,7 +8,7 @@ const Quote = () => {
                     <h2 className='text-[28px] md:text-[48px] font-bold h-[72px] truncate '>Try Our Best Selling</h2>
                     <div className='flex gap-5 md:gap-[30px] lg:gap-[56px] h-[80px] relative md:h-[99px]  justify-between items-center '>
                     <div className=' w-[3px] h-full bg-opacity-70 bg-[#4A1D1F]' />
-                    <div className='text-[#272727] text-[12px] md:text-[22px] lg:w-[450px] xl:w-[640px]  flex justify-end  '><p className=' w-fit'>Here’s our best creations that everyone loves. Lightness and sweetness of the cake make you want more and more. Start from cake, bread and other creations.</p></div>
+                    <div className='text-[#272727] text-[12px] md:text-[22px] lg:w-[450px] xl:w-[640px]  flex justify-end  '><p className=' w-fit text-base md:text-lg'>Here’s our best creations that everyone loves. Lightness and sweetness of the cake make you want more and more. Start from cake, bread and other creations.</p></div>
                     </div>
                 </div>
             </div>
