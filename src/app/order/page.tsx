6ab8b2511cb0ca64a1698e342e77bbc6859b595a
Sharoon-Masih <sharoonmasih43@gmail.com>
2024-06-19 17:56:29 +0500

    update for line_items

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -5,7 +5,7 @@ const OrderPage = async () => {
 // const orderData=await getAllOrders()
 return (
 <main >
-<section className='${poppin.className} text-[#4A1D1F] relative overflow-hidden height' >
+<section className='${poppin.className} text-[#4A1D1F] relative overflow-hidden height ' >
     <div className='2xl:max-w-[1280px] mx-auto h-full py-[30px] lg:py-[40px] px-4 '>
         <span className={`text-[18px] sm:text-[20px] md:text-[25px] font-semibold tracking-[9%] text-[#4A1D1F] uppercase truncate`}>
             My Orders
